9b95a8db4adc2c6272991dd94f947bcf3edbfec8
Kimmingyu <d2rkjc38@gmail.com>
2025-11-01 19:33:12 +0900

    play_mode.py handleevent함수 추가

--- play_mode.py
+++ play_mode.py
@@ -1,8 +1,18 @@
 from pico2d import *
 from map import Map
 import game_framework
+import game_world
 
 select_map = 0
+
+def handle_events():
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                game_framework.quit()
 def init():
     pass
 
