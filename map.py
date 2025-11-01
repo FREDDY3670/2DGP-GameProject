e03af0f28abb8e17280a3fe46caeb01d89c2ec82
Kimmingyu <d2rkjc38@gmail.com>
2025-11-01 19:23:20 +0900

    배경, 타일 이미지 추가

--- map.py
+++ map.py
@@ -1,4 +1,6 @@
 from play_mode import select_map
+from pico2d import *
 class Map:
     def __init__(self):
-        pass
+        self.bg_image = load_image('Gray-BG.png')
+        self.tile_image = load_image('tile_map1.png')
