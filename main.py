8c4a40bae605fcc4888f26848533ee18114864fa
Kimmingyu <d2rkjc38@gmail.com>
2025-11-01 19:38:42 +0900

    select_map을 Map클래스 생성자함수의 인자로 변경, main함수에 play_mode를 실행시키는 코드 추가

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 from pico2d import *
+import play_mode as start_mode
+import game_framework
+
+
+open_canvas()
+game_framework.run(start_mode)
+close_canvas()
 
--- map.py
+++ map.py
@@ -1,8 +1,7 @@
-from play_mode import select_map
 from pico2d import *
 
 class Map:
-    def __init__(self):
+    def __init__(self,select_map = 0):
         self.bg_image = load_image('Gray-BG.png')
         self.tile_image = load_image('tile_map1.png')
     def draw(self):
