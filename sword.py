b17788861de225146a517df2b97f20871e3ff303
Kimmingyu <d2rkjc38@gmail.com>
2025-11-09 19:50:33 +0900

    Sword 이미지 로드

--- sword.py
+++ sword.py
@@ -10,4 +10,17 @@ class Idle:
     pass
 
 class Sword:
-    pass
+    image_idle = None
+    image_run = None
+    image_ia = None
+    image_ra = None
+
+    def __init__(self):
+        if Sword.image_idle == None:
+            Sword.image_idle = load_image('SwordIdle01-sheet.png')
+        if Sword.image_run == None:
+            Sword.image_run = load_image('SwordRun01_right.png')
+        if Sword.image_ia == None:
+            Sword.image_ia = load_image('swordcombo.png')
+        if Sword.image_ra == None:
+            Sword.image_ra = load_image('SwordRunSlash01_right.png')
