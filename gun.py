c94e4f6de3ef8671b7ec0a432be81efb9f2d851e
Kimmingyu <d2rkjc38@gmail.com>
2025-11-09 20:31:54 +0900

    gun 이미지 로드

--- gun.py
+++ gun.py
@@ -1,3 +1,4 @@
+from pico2d import load_image
 
 class Idle:
     pass
@@ -6,4 +7,16 @@ class Run:
     pass
 
 class Gun:
-    pass
+    def __init__(self):
+        image_idle = None
+        image_run = None
+        image_ia = None
+        image_ra = None
+        if Gun.image_idle == None:
+            Gun.image_idle = load_image('GunAim.png')
+        if Gun.image_run == None:
+            Gun.image_run = load_image('GunRun.png')
+        if Gun.image_ia == None:
+            Gun.image_ia = load_image('GunFire.png')
+        if Gun.image_ra == None:
+            Gun.image_ra = load_image('GunRunFire01-sheet.png')
