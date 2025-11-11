d202a6871e207e3b635860628f2070f12076ad54
Kimmingyu <d2rkjc38@gmail.com>
2025-11-11 21:28:49 +0900

    각 이미지 로드

--- select_weapon.py
+++ select_weapon.py
@@ -5,3 +5,18 @@ import play_mode
 
 player1_weapon = 0
 player2_weapon = 0
+
+def init():
+    image_p = None
+    image_s = None
+    image_b = None
+    image_g = None
+    global image_p, image_s, image_b, image_g
+    if image_p == None:
+        image_p = load_image('Punch0101-sheet.png')
+    if image_s == None:
+        image_s = load_image('swordcombo.png')
+    if image_b == None:
+        image_b = load_image('BowFire01-sheet.png')
+    if image_g == None:
+        image_g = load_image('GunFire.png')
