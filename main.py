2512c87abfdeafc40dceabcef2f4c261609da879
Kimmingyu <d2rkjc38@gmail.com>
2025-11-11 21:46:55 +0900

    이미지 4개 띄우기, 시작모드 무기선택창으로 수정

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pico2d import *
-import play_mode as start_mode
+import select_weapon as start_mode
 import game_framework
 
 
--- select_weapon.py
+++ select_weapon.py
@@ -12,13 +12,14 @@ frame_4 = 0
 
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+image_p = None
+image_s = None
+image_b = None
+image_g = None
+bg = None
 
 def init():
-    image_p = None
-    image_s = None
-    image_b = None
-    image_g = None
-    global image_p, image_s, image_b, image_g
+    global image_p, image_s, image_b, image_g,bg
     if image_p == None:
         image_p = load_image('Punch0101-sheet.png')
     if image_s == None:
@@ -27,18 +28,35 @@ def init():
         image_b = load_image('BowFire01-sheet.png')
     if image_g == None:
         image_g = load_image('GunFire.png')
+    if bg == None:
+        bg = load_image('Gray-BG.png')
 
 def update():
-    pass
+    global frame_1, frame_2, frame_3, frame_4
+    frame_1 = (frame_1 + 6 * ACTION_PER_TIME * game_framework.frame_time) % 6
+    frame_2 = (frame_2 + 6 * ACTION_PER_TIME * game_framework.frame_time) % 6
+    frame_3 = (frame_3 + 5 * ACTION_PER_TIME * game_framework.frame_time) % 5
+    frame_4 = (frame_4 + 5 * ACTION_PER_TIME * game_framework.frame_time) % 5
 
 def finish():
     pass
 
 def draw():
-    pass
+    clear_canvas()
+    bg.draw(800,450)
+    image_p.clip_draw(int(frame_1) * 96, 0, 96, 84, 200, 400, 150, 150)
+    image_s.clip_draw(int(frame_2) * 96, 0, 96, 84, 400, 400, 150, 150)
+    image_b.clip_draw(int(frame_3) * 96, 0, 96, 84, 600, 400, 150, 150)
+    image_g.clip_draw(int(frame_4) * 96, 0, 96, 84, 800, 400, 150, 150)
+    update_canvas()
 
 def handle_events():
-    pass
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
 
 def pause():
     pass
