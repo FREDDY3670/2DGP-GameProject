bbeea60a366b47c2f9670226a37b0366a34b606e
Kimmingyu <d2rkjc38@gmail.com>
2025-11-14 11:54:04 +0900

    Punch클래스 생성자함수 기본변수 초기화

--- punch.py
+++ punch.py
@@ -10,5 +10,26 @@ class Idle:
     pass
 
 class Punch:
+    image_ia1 = None
+    image_ia2 = None
+    image_ia3 = None
     def __init__(self, player_id = 1, start_x = 100, start_y = 180):
+        if Punch.image_ia1 == None:
+            Punch.image_ia1 = load_image('Punch0101-sheet.png')
+        if Punch.image_ia2 == None:
+            Punch.image_ia2 = load_image('Punch0201-sheet.png')
+        if Punch.image_ia3 == None:
+            Punch.image_ia3 = load_image('Punch0301-sheet.png')
+        self.player_id = player_id
+        self.x, self.y = start_x, start_y
+        self.frame = 0
+        self.face_dir = 1 if player_id == 1 else -1
+
+    def update(self):
+        pass
+
+    def handle_event(self, event):
+        pass
+
+    def draw(self):
         pass
