92746607b724cd0e88b15746883441490d05034b
Kimmingyu <d2rkjc38@gmail.com>
2025-11-16 22:00:57 +0900

    타일 히트박스 추가

--- tile.py
+++ tile.py
@@ -1,12 +1,19 @@
-from pico2d import load_image
+from pico2d import load_image, draw_rectangle
+
 
 class Tile:
+    tile_image = None
     def __init__(self, select_map=0):
-        self.tile_image = load_image('tile_map1.png')
+        if Tile.tile_image == None:
+            Tile.tile_image = load_image('tile_map1.png')
+        self.x, self.y = 100,100
+
+    def get_bb(self):
+        return self.x - 25, self.y - 25, self.x + 25, self.y + 25
 
     def draw(self):
-        for x in range(32):
-            self.tile_image.draw(x*50,50,50,50)
+        draw_rectangle(*self.get_bb())
+        self.tile_image.draw(self.x, self.y,50,50)
 
     def update(self):
         pass
